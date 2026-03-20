09ba66b23d2b244377050df904aff19989c90c02
5peak2me <461260911@qq.com>
2026-03-20 21:29:48 +0800

    编译问题

--- app/src/main/java/com/github/speak2me/app/compose/map/MapComposeActivity.kt
+++ app/src/main/java/com/github/speak2me/app/compose/map/MapComposeActivity.kt
@@ -19,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.github.speak2me.app.compose.map.ui.theme.MapComposeTheme
 
@@ -84,7 +83,7 @@ private fun MapComposeTopBar(
         contentAlignment = Alignment.CenterStart
     ) {
         Text(
-            text = stringResource(R.string.map_compose_activity),
+            text = "离线地图",
             modifier = Modifier.padding(horizontal = 16.dp),
             style = MaterialTheme.typography.titleMedium,
             color = MaterialTheme.colorScheme.onBackground
